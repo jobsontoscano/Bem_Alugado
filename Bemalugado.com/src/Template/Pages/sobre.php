a4fefcf7e7fc271b9e6a35a5cfd66ad9dbb92887
ViniciusGuedes <viniciusocamisa100@gmail.com>
2017-12-12 09:50:15 -0300

    sobre page

---
+++ Bemalugado.com/src/Template/Pages/sobre.php
@@ -0,0 +1,6 @@
+<div>
+	<h1>BEMALUGADO.COM</h1>
+	Esta plataforma esta sendo desenvolvida por uma equipe de alunos do Instituto Federal de Pernambuco - IFPE dos Campus Olinda, Igarassu e Paulista.
+
+	O BemAlugado.com é uma plataforma voltada para o comerciante que busca o local ideal para montar seu negócio. Para isso ele contará com informações, onde somos os únicos no mercado , como por exemplo dados de uma região específica ao qual o cliente se interessou, dados esses que poderão ser voltados para cultura, características demográficas, negócio similares que estão próximos aos locais desejados. 
+</div>
